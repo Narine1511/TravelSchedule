802ee556d08987c33a1f94e77b7c305f859fabcf
Наринэ <narine.ovsepian@yandex.ru>
2026-08-29 16:24:24 +0700

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Наринэ  Овсепян on 29.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
